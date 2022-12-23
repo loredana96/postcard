dd18b5f8ae0e4c0c94c62861dcfb948093fc9bd7
loredana96 <loredanatodoran15@gmail.com>
2022-12-23 04:06:15 +0200

    implement postcard form and image load

--- postcard-app/src/app/app.module.ts
+++ postcard-app/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
 import { PostcardComponent } from './postcard/postcard.component';
+import { HttpClientModule } from '@angular/common/http'; 
+import { ImageCropperModule } from 'ngx-image-cropper';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,8 @@ import { PostcardComponent } from './postcard/postcard.component';
     PostcardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImageCropperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- postcard-app/src/app/postcard/postcard.component.ts
+++ postcard-app/src/app/postcard/postcard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-postcard',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./postcard.component.scss']
 })
 export class PostcardComponent {
+  show = true;
+  imgChangeEvt: any = '';
+  cropImgPreview: any = '';
+  onFileChange(event: any): void {
+    this.show = true;
+    this.imgChangeEvt = event;
+  }
+  cropImg(e: ImageCroppedEvent) {
+      this.cropImgPreview = e.base64;
+  }
+  imgLoad() {
+      // display cropper tool
+  }
+  initCropper() {
+      // init cropper
+  }
+  
+  imgFailed() {
+      // error msg
+  }
 
+  hideImage() {
+    this.show = false;
+  }
 }
